f84b1147a6168b754579c04227964e069191f32e
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-11-17 05:51:29 +0100

    Update env.c

--- env.c
+++ env.c
@@ -1,29 +1,15 @@
 #include "builtins.h"
 
-
-
 /**
+ * bin_env - Implementation for the env builtin
  *
- *  * bin_env - Implementation for the env builtin
- *
- *   *
- *
- *    * @info: General information about the shell
- *
- *     * @arguments: Command
- *
- *      **/
-
+ * @info: General information about the shell
+ * @arguments: Command
+ **/
 void bin_env(general_t *info, char **arguments)
-
 {
+	(void) info;
+	(void) arguments;
 
-		(void) info;
-
-			(void) arguments;
-
-
-
-				get_full_env();
-
+	get_full_env();
 }
