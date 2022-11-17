fb69cdfa48967956155eec8f8bc75a9c011a87c6
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-11-17 05:47:03 +0100

    Update free.c

--- free.c
+++ free.c
@@ -1,63 +1,32 @@
 #include "memory.h"
 
-
-
 /**
+ * free_memory_p - Free a pointer
  *
- *  * free_memory_p - Free a pointer
- *
- *   *
- *
- *    * @ptr: Pointer to free
- *
- *     **/
-
+ * @ptr: Pointer to free
+ **/
 void free_memory_p(void *ptr)
-
 {
+	if (ptr != NULL)
+	{
+		free(ptr);
+		ptr = NULL;
+	}
 
-		if (ptr != NULL)
-
-				{
-
-							free(ptr);
-
-									ptr = NULL;
-
-										}
-
-
-
-			ptr = NULL;
-
+	ptr = NULL;
 }
 
-
-
 /**
+ * free_memory_pp - Free a double pointer
  *
- *  * free_memory_pp - Free a double pointer
- *
- *   *
- *
- *    * @ptr: Double pointer to free
- *
- *     **/
-
+ * @ptr: Double pointer to free
+ **/
 void free_memory_pp(void **ptr)
-
 {
+	void **tmp;
 
-		void **tmp;
+	for (tmp = ptr; *tmp != NULL; tmp++)
+		free_memory_p(*tmp);
 
-
-
-			for (tmp = ptr; *tmp != NULL; tmp++)
-
-						free_memory_p(*tmp);
-
-
-
-				free_memory_p(ptr);
-
+	free_memory_p(ptr);
 }
