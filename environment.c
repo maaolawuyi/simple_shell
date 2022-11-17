425708574fc52c159847d5e92eb5308e55444fa2
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-11-17 05:50:51 +0100

    Update environment.c

--- environment.c
+++ environment.c
@@ -1,271 +1,136 @@
 #include "commands.h"
-
 #include "general.h"
-
 #include "text.h"
-
 #include <string.h>
-
 #include <sys/cdefs.h>
-
 #include <unistd.h>
 
-
-
 /**
+ * _getenv - Get a environment variable
  *
- *  * _getenv - Get a environment variable
+ * @name: Environment variable to get
  *
- *   *
- *
- *    * @name: Environment variable to get
- *
- *     *
- *
- *      * Return: On success value of @name
- *
- *       * On error, NULL
- *
- *        **/
-
+ * Return: On success value of @name
+ * On error, NULL
+ **/
 char *_getenv(const char *name)
-
 {
+	char **env;
+	char *aux, *token, *value;
+	int size;
 
-		char **env;
+	size = _strlen((char *) name);
 
-			char *aux, *token, *value;
+	for (env = environ; *env; ++env)
+	{
+		aux = _strdup(*env);
 
-				int size;
+		token = strtok(aux, "=");
+		if (token == NULL)
+		{
+			free(aux);
+			return (NULL);
+		}
 
+		if (_strlen(token) != size)
+		{
+			free(aux);
+			continue;
+		}
 
+		if (_strcmp((char *) name, aux) == 0)
+		{
+			token = strtok(NULL, "=");
+			value = _strdup(token);
 
-					size = _strlen((char *) name);
+			free(aux);
+			return (value);
+		}
 
+		free(aux);
+	}
 
-
-						for (env = environ; *env; ++env)
-
-								{
-
-											aux = _strdup(*env);
-
-
-
-													token = strtok(aux, "=");
-
-															if (token == NULL)
-
-																		{
-
-																						free(aux);
-
-																									return (NULL);
-
-																											}
-
-
-
-																	if (_strlen(token) != size)
-
-																				{
-
-																								free(aux);
-
-																											continue;
-
-																													}
-
-
-
-																			if (_strcmp((char *) name, aux) == 0)
-
-																						{
-
-																										token = strtok(NULL, "=");
-
-																													value = _strdup(token);
-
-
-
-																																free(aux);
-
-																																			return (value);
-
-																																					}
-
-
-
-																					free(aux);
-
-																						}
-
-
-
-							return (NULL);
-
+	return (NULL);
 }
 
-
-
 /**
+ * which - Find the directory needed
  *
- *  * which - Find the directory needed
+ * @filename: Command received
+ * @info: General info about the shell
  *
- *   *
- *
- *    * @filename: Command received
- *
- *     * @info: General info about the shell
- *
- *      *
- *
- *       * Return: pointer string with found path or NULL in failure.
- *
- *        */
-
+ * Return: pointer string with found path or NULL in failure.
+ */
 char *which(char *filename, general_t *info)
-
 {
+	char *path, *tmp_path, *token;
+	char *slash;
+	int size;
 
-		char *path, *tmp_path, *token;
+	(void) info;
 
-			char *slash;
+	path = _getenv("PATH");
+	if (path == NULL)
+		return (NULL);
 
-				int size;
+	token = strtok(path, ":");
 
+	size = _strlen(filename) + 2;
+	slash = malloc(size * sizeof(char));
+	slash = _strcpy(slash, "/");
+	slash = _strcat(slash, filename);
 
+	while (token != NULL)
+	{
+		tmp_path = malloc(_strlen(token) + size);
+		tmp_path = _strcpy(tmp_path, token);
+		tmp_path = _strcat(tmp_path, slash);
 
-					(void) info;
+		if (is_executable(tmp_path) == PERMISSIONS)
+		{
+			free(slash);
+			free(path);
+			return (tmp_path);
+		}
+		token = strtok(NULL, ":");
 
+		free(tmp_path);
+	}
 
+	free(path);
+	free(slash);
 
-						path = _getenv("PATH");
-
-							if (path == NULL)
-
-										return (NULL);
-
-
-
-								token = strtok(path, ":");
-
-
-
-									size = _strlen(filename) + 2;
-
-										slash = malloc(size * sizeof(char));
-
-											slash = _strcpy(slash, "/");
-
-												slash = _strcat(slash, filename);
-
-
-
-													while (token != NULL)
-
-															{
-
-																		tmp_path = malloc(_strlen(token) + size);
-
-																				tmp_path = _strcpy(tmp_path, token);
-
-																						tmp_path = _strcat(tmp_path, slash);
-
-
-
-																								if (is_executable(tmp_path) == PERMISSIONS)
-
-																											{
-
-																															free(slash);
-
-																																		free(path);
-
-																																					return (tmp_path);
-
-																																							}
-
-																										token = strtok(NULL, ":");
-
-
-
-																												free(tmp_path);
-
-																													}
-
-
-
-														free(path);
-
-															free(slash);
-
-
-
-																return (NULL);
-
+	return (NULL);
 }
 
-
-
 /**
+ * is_current_path -	Check the order of the path
  *
- *  * is_current_path -	Check the order of the path
- *
- *   *
- *
- *    * @path: PATH to check
- *
- *     * @info: General infor about the shell
- *
- *      **/
-
+ * @path: PATH to check
+ * @info: General infor about the shell
+ **/
 void is_current_path(char *path, general_t *info)
-
 {
+	info->is_current_path = _FALSE;
 
-		info->is_current_path = _FALSE;
+	if (path == NULL)
+		return;
 
-
-
-			if (path == NULL)
-
-						return;
-
-
-
-				if (path[0] == ':')
-
-							info->is_current_path = _TRUE;
-
+	if (path[0] == ':')
+		info->is_current_path = _TRUE;
 }
 
-
-
 /**
- *
- *  * get_full_env - Get all the environment
- *
- *   **/
-
+ * get_full_env - Get all the environment
+ **/
 void get_full_env(void)
-
 {
+	char **tmp;
+	int i;
 
-		char **tmp;
-
-			int i;
-
-
-
-				for (i = 0, tmp = environ; tmp[i] != NULL; i++)
-
-						{
-
-									print(tmp[i]);
-
-											_putchar('\n');
-
-												}
-
+	for (i = 0, tmp = environ; tmp[i] != NULL; i++)
+	{
+		print(tmp[i]);
+		_putchar('\n');
+	}
 }
